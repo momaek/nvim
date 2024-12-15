04b802f3283e836d358762cb2530002b5f4f5add
momaek <momaek17@gmail.com>
2024-12-15 16:55:13 +0800

    add keymap, lsp, options

---
+++ lua/keymaps.lua
@@ -0,0 +1,21 @@
+-- Define common options
+local opts = {
+	noremap = true, -- non-recursive
+	silent = true, -- do not show message
+}
+
+-----------------
+-- Normal mode --
+-----------------
+
+-- Hint: see `:h vim.map.set()`
+-- Better window navigation
+vim.keymap.set("n", "<C-h>", "<C-w>h", opts)
+vim.keymap.set("n", "<C-j>", "<C-w>j", opts)
+vim.keymap.set("n", "<C-k>", "<C-w>k", opts)
+vim.keymap.set("n", "<C-l>", "<C-w>l", opts)
+
+vim.keymap.set('i', 'jj', '<ESC>', opts)
+
+vim.keymap.set('n', 'sw', ':w!<CR>', opts)
+vim.keymap.set('i', 'sw', '<ESC>:w!<CR>', opts)
---
+++ lua/options.lua
@@ -0,0 +1,39 @@
+-- Hint: use `:h <option>` to figure out the meaning if needed
+vim.opt.clipboard = "unnamedplus" -- use system clipboard
+vim.opt.completeopt = { "menu", "menuone", "noselect" }
+vim.opt.mouse = "a" -- allow the mouse to be used in Nvim
+vim.opt.scrolloff = 10 -- no less than 10 lines even if you keep scrolling down
+
+-- Tab
+vim.opt.tabstop = 4 -- the number of visual spaces per TAB
+vim.opt.softtabstop = 4 -- number of spacesin tab when editing
+vim.opt.shiftwidth = 4 -- insert 4 spaces on a tab
+vim.opt.expandtab = true -- tabs are spaces, mainly because of python
+
+-- UI config
+vim.opt.number = true -- show absolute number
+vim.opt.relativenumber = true -- add numbers to each line on the left side
+vim.opt.cursorline = true -- highlight cursor line underneath the cursor horizontally
+vim.opt.splitbelow = true -- open new vertical split bottom
+vim.opt.splitright = true -- open new horizontal splits right
+vim.opt.termguicolors = true -- enable 24-bit RGB color in the TUI
+vim.opt.showmode = false -- we are experienced, and we don't need the "-- INSERT --" mode hint
+
+-- Searching
+vim.opt.incsearch = true -- search as characters are entered
+vim.opt.hlsearch = false -- do not highlight matches
+vim.opt.ignorecase = true -- ignore case in searches by default
+vim.opt.smartcase = true -- but make it case sensitive if an uppercase is entered
+
+-- For nvim-tree
+-- disable netrw at the very start of your init.lua (strongly advised)
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+vim.g.mapleader = ","
+
+vim.opt.title = true
+vim.opt.autoindent = true
+vim.opt.autoread = true
+vim.opt.encoding = 'utf-8'
+vim.opt.backspace = 'eol,start,indent'
